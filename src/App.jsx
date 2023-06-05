58b01591c72384b48e25569f0f4c5576527c520f
Moritz Stückler <moritz@bitbetter.de>
2023-06-05 20:23:44 +0200

    feat: mapbox working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Map from 'react-map-gl';
 
 function App() {
   return (
-    <>
-      Hello Mapbox
-    </>
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+    />
   )
 }
 
